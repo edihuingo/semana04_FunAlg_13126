ef4c1264391caeeafad8a1759ec81a492fd4ab34
gian <gian2345qn123456>
2025-04-16 17:35:59 -0500

    guardar todo

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, nota4, prom;
+            Console.WriteLine("Ingresar la nota T1(10%): ");
+            nota1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar la nota T2(20%): ");
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar la nota T3(30%): ");
+            nota3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar la nota EF(40%): ");
+            nota4 = float.Parse(Console.ReadLine());
+
+            prom = (nota1 * 0.10) + (nota2 * 0.20) + (nota3 * 0.30) + (nota4 * 0.40);
+
+            Console.WriteLine("Promedio: " + prom);
+            if (prom > 11.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Aprobado");
+            }
+            else
+            { 
+                Console.ForegroundColor=ConsoleColor.Red;
+                Console.WriteLine("Desaprobado");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
--- Semana04/Program.cs
+++ Semana04/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,39 @@ namespace Semana04
     {
         static void Main(string[] args)
         {
+            //ax^2 + bx + c
+            //2 soluciones 
+            //disc> 0 -> 2 sol.
+            //disc= 0 -> 1 sol.
+            //disc< 0 -> 0 sol.
+            int a, b, c, disc;
+            double x1, x2;
+            Console.Write("Ingrese el valor de a: ");
+            a = int.Parse(Console.ReadLine());
 
+            Console.Write("Ingrese el valor de b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de c: ");
+            c = int.Parse(Console.ReadLine());
 
+            disc = (b * b) - (4 * a * b * c);
+            if (disc > 0)
+            {
+                x1 = (-b + Math.Sqrt(disc)) / 2 * a;
+                x2 = (-b - Math.Sqrt(disc)) / 2 * a;
+                Console.WriteLine("X1: " + x1);
+                Console.WriteLine("X2: " + x2);
+            } else
+            { 
+                if (disc==0)
+                {
+                    x1 = -b / 2 * a;
+                    Console.WriteLine("X1: " + x1);
+                } else
+                {
+                    Console.WriteLine("No tiene soluciones");
+                }
+            }
         }
     }
 }
